7d8ff5a7cdaf9c2754abda2d241689178809bf78
Larissa <larissachg@gmail.com>
2024-09-19 20:38:04 -0400

    implementacion base de datos

--- src/api/services/clients.service.ts
+++ src/api/services/clients.service.ts
@@ -1,7 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const getClients = async () => {
- 
+  const products = await prisma.productos.findMany()
 
-  return 'Hola Mundo';
+  return products;
 };
 
